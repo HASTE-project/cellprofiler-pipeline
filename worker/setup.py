0955e26a3cd9a52b98c3434750664eea75386bfc
Ben Blamey <blamey.ben@gmail.com>
2021-01-12 22:19:48 +0100

    missed a comma

--- worker/setup.py
+++ worker/setup.py
@@ -12,7 +12,7 @@ setup(name='haste_pipeline_worker',
       author='Ben Blamey',
       author_email='ben.blamey@it.uu.se',
       install_requires=[
-          'numpy'
+          'numpy',
           'pika',
           'haste-storage-client'
       ],
