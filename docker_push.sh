8487af44af8d77e0fbb42134668d843b667db1ca
Ben Blamey <blamey.ben@gmail.com>
2021-01-25 06:20:31 +0100

    fix docker, hsc versions for reproducibility

--- docker_push.sh
+++ docker_push.sh
@@ -4,5 +4,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 #docker push benblamey/haste_pipeline_worker_base:latest
-docker push benblamey/haste_pipeline_worker:latest
-docker push benblamey/haste_pipeline_client:latest
+docker push benblamey/haste_pipeline_worker:v3
+docker push benblamey/haste_pipeline_client:v1
